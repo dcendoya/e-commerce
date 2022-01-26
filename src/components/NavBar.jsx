a00b2656a8ad9ff359ac0490d1175e213c46b3d7
Denise Cendoya <denisecendoya@gmail.com>
2022-01-26 19:24:28 -0300

    NavBar con links a categorÃias de productos

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -8,8 +8,9 @@ export default function NavBar (){
             <div style={{display: "flex", flexDirection: "row"}}>
                 <div style={{padding: "30px", justifyContent: "center", fontWeight: "bolder"}}><Link to={"/"} style={{textDecoration: "none", color:"black"}}>Marca</Link></div>
                 <ul style={{listStyleType: "none", margin: "0px", padding: "30px", justifyContent: "center"}}>
-                    <li style={{display: "inline", padding: "10px"}}><a href="#" style={{textDecoration: "none", color: "black", fontWeight: "lighter"}}>Home</a></li>
-                    <li style={{display: "inline", padding: "10px"}}><a href="#" style={{textDecoration: "none", color: "black", fontWeight: "lighter"}}>Productos</a></li>
+                    <li style={{display: "inline", padding: "10px"}}><Link to={"/"} style={{textDecoration: "none", color: "black", fontWeight: "lighter"}}>Home</Link></li>
+                    <li style={{display: "inline", padding: "10px"}}><Link to={"/catalogo/tops"} style={{textDecoration: "none", color: "black", fontWeight: "lighter"}}>Top Wear</Link></li>
+                    <li style={{display: "inline", padding: "10px"}}><Link to={"/catalogo/bottoms"} style={{textDecoration: "none", color: "black", fontWeight: "lighter"}}>Bottom Wear</Link></li>
                     <li style={{display: "inline", padding: "10px"}}><a href="#" style={{textDecoration: "none", color: "black", fontWeight: "lighter"}}>Contacto</a></li>   
                 </ul>
                 <CartIcon style={{display: "inline"}}/>
