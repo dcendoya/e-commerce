31b61ac9c368feb645b84993e174ae78608c7ece
Denise Cendoya <denisecendoya@gmail.com>
2022-02-17 18:59:30 -0300

    NavBar css

--- src/components/header/NavBar.jsx
+++ src/components/header/NavBar.jsx
@@ -1,16 +1,17 @@
 import React from "react";
 import { Link } from 'react-router-dom/cjs/react-router-dom.min';
 import CartIcon from "./CartWidget";
+import "./NavBar.css";
 
 export default function NavBar (){
     return (
         <>
-            <div style={{display: "flex", flexDirection: "row"}}>
-                <div style={{padding: "30px", justifyContent: "center", fontWeight: "bolder"}}><Link to={"/"} style={{textDecoration: "none", color:"black"}}>Pure™</Link></div>
-                <ul style={{listStyleType: "none", margin: "0px", padding: "30px", justifyContent: "center"}}>
-                    <li style={{display: "inline", padding: "10px"}}><Link to={"/"} style={{textDecoration: "none", color: "black", fontWeight: "lighter"}}>Home</Link></li>
-                    <li style={{display: "inline", padding: "10px"}}><Link to={"/catalogo/tops"} style={{textDecoration: "none", color: "black", fontWeight: "lighter"}}>Top Wear</Link></li>
-                    <li style={{display: "inline", padding: "10px"}}><Link to={"/catalogo/bottoms"} style={{textDecoration: "none", color: "black", fontWeight: "lighter"}}>Bottom Wear</Link></li>
+            <div className="navBarCointainer">
+                <div className="brandIconContainer"><Link to={"/"} className="brandIcon">Pure™</Link></div>
+                <ul className="navList">
+                    <li className="listItem"><Link to={"/"} className="itemLink">Home</Link></li>
+                    <li className="listItem"><Link to={"/catalogo/tops"} className="itemLink">Top Wear</Link></li>
+                    <li className="listItem"><Link to={"/catalogo/bottoms"} className="itemLink">Bottom Wear</Link></li>
                 </ul>
                 <CartIcon style={{display: "inline"}}/>
             </div>
